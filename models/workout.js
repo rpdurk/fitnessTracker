40a8858d9bc4a1069d9a98b806568a89f6a1b7de
Ryan Durk <rpdurk@gmail.com>
2020-11-25 11:19:20 -0800

    updated workout schema

--- models/workout.js
+++ models/workout.js
@@ -3,9 +3,48 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema(
   {
-    day:
-})
+    day: 
+    {
+      type: Date,
+      default: () => new Date(),
+    },
 
+    exercises: [ 
+    {
+      type: {
+      type: String,
+      trim: true,
+      required: 'Please enter the type of exercise.',
+      },
+      name: {
+      type: String,
+      trim: true,
+      required: 'Please enter the exercise name.',
+    },
+      duration: {
+        type: Number,
+        required: 'Please enter the duration of exercise.'
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    }
+  ]
+  }
+)
+const Workout = model('workout', workoutSchema);
+
+module.exports = Workout;
+// example of what would be inserted into database
 // let workoutSeed = [
 //   {
 //     day: new Date().setDate(new Date().getDate()-10),
@@ -21,32 +60,6 @@ const workoutSchema = new Schema(
 //     ]
 //   },
 //   {
-//     day: new Date().setDate(new Date().getDate()-9),
-//     exercises: [
-//       {
-//         type: "resistance",
-//         name: "Lateral Pull",
-//         duration: 20,
-//         weight: 300,
-//         reps: 10,
-//         sets: 4
-//       }
-//     ]
-//   },
-//   {
-//     day: new Date().setDate(new Date().getDate()-8),
-//     exercises: [
-//       {
-//         type: "resistance",
-//         name: "Push Press",
-//         duration: 25,
-//         weight: 185,
-//         reps: 8,
-//         sets: 4
-//       }
-//     ]
-//   },
-//   {
 //     day: new Date().setDate(new Date().getDate()-7),
 //     exercises: [
 //       {
@@ -56,81 +69,5 @@ const workoutSchema = new Schema(
 //         distance: 4
 //       }
 //     ]
-//   },
-//   {
-//     day: new Date().setDate(new Date().getDate()-6),
-//     exercises: [
-//       {
-//         type: "resistance",
-//         name: "Bench Press",
-//         duration: 20,
-//         weight: 285,
-//         reps: 10,
-//         sets: 4
-//       }
-//     ]
-//   },
-//   {
-//     day: new Date().setDate(new Date().getDate()-5),
-//     exercises: [
-//       {
-//         type: "resistance",
-//         name: "Bench Press",
-//         duration: 20,
-//         weight: 300,
-//         reps: 10,
-//         sets: 4
-//       }
-//     ]
-//   },
-//   {
-//     day: new Date(new Date().setDate(new Date().getDate() - 4)),
-//     exercises: [
-//       {
-//         type: "resistance",
-//         name: "Quad Press",
-//         duration: 30,
-//         weight: 300,
-//         reps: 10,
-//         sets: 4
-//       }
-//     ]
-//   },
-//   {
-//     day: new Date(new Date().setDate(new Date().getDate() - 3)),
-//     exercises: [
-//       {
-//         type: "resistance",
-//         name: "Bench Press",
-//         duration: 20,
-//         weight: 300,
-//         reps: 10,
-//         sets: 4
-//       }
-//     ]
-//   },
-//   {
-//     day: new Date(new Date().setDate(new Date().getDate() - 2)),
-//     exercises: [
-//       {
-//         type: "resistance",
-//         name: "Military Press",
-//         duration: 20,
-//         weight: 300,
-//         reps: 10,
-//         sets: 4
-//       }
-//     ]
 //   }
 // ];
-
-// db.Workout.deleteMany({})
-//   .then(() => db.Workout.collection.insertMany(workoutSeed))
-//   .then(data => {
-//     console.log(data.result.n + " records inserted!");
-//     process.exit(0);
-//   })
-//   .catch(err => {
-//     console.error(err);
-//     process.exit(1);
-//   });
